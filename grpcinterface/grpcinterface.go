9f7d85b801753f517665cbb0cd2332654a983188
iznauy <161250220@smail.nju.edu.cn>
2020-07-18 09:51:18 +0800

    优化批量插入接口性能

--- grpcinterface/grpcinterface.go
+++ grpcinterface/grpcinterface.go
@@ -2,11 +2,13 @@ package grpcinterface
 
 import (
 	"context"
+	"errors"
 	btrdb2 "github.com/iznauy/BTrDB/btrdbd"
 	"github.com/iznauy/BTrDB/qtree"
 	"github.com/op/go-logging"
 	"github.com/pborman/uuid"
 	"google.golang.org/grpc"
+	"math"
 	"net"
 )
 
@@ -26,10 +28,14 @@ var ErrBadUUID = &Status{
 	Msg:  "Invalid uuid",
 }
 
+var BadUUID = errors.New("Invalid uuid")
+
 var ErrBadTimes = &Status{
 	Code: 402,
 	Msg:  "Invalid time range",
 }
+
+var BadTimes = errors.New("Invalid time range")
 
 type GRPCInterface struct {
 	q *btrdb2.Quasar
@@ -79,9 +85,9 @@ func (g *GRPCInterface) Insert(ctx context.Context, req *InsertRequest) (*Insert
 	}, nil
 }
 
-func (g *GRPCInterface) BatchInsert(ctx context.Context, batchReq *BatchInsertRequest) (*BatchInsertResponse, error) {
-	recordsMap := make(map[string][]qtree.Record, len(batchReq.Inserts))
-	for _, req := range batchReq.Inserts {
+func (g *GRPCInterface) batchInsert(insertReqs []*InsertRequest) { // 同步方法
+	recordsMap := make(map[string][]qtree.Record, len(insertReqs))
+	for _, req := range insertReqs {
 		records := make([]qtree.Record, 0, len(req.Values))
 		for _, val := range req.Values {
 			if val == nil {
@@ -91,29 +97,54 @@ func (g *GRPCInterface) BatchInsert(ctx context.Context, batchReq *BatchInsertRe
 				Time: val.Time,
 				Val:  val.Value,
 			}
-			if !checkTime(record.Time) {
-				return &BatchInsertResponse{
-					Status: ErrBadTimes,
-				}, nil
-			}
 			records = append(records, record)
-		}
-		_, err := uuid.ParseBytes(req.Uuid)
-		if err != nil {
-			log.Fatal("[Insert] invalid uuid: %v", err)
-			return &BatchInsertResponse{
-				Status: ErrBadUUID,
-			}, nil
 		}
 		recordsMap[string(req.Uuid)] = records
 	}
+	for uid, records := range recordsMap {
+		id := uuid.Parse(uid)
+		g.q.InsertValues(id, records)
+	}
+}
 
-	go func(recordsMap map[string][]qtree.Record) { // 完全异步化，试试效果
-		for uid, records := range recordsMap {
-			id := uuid.Parse(uid)
-			g.q.InsertValues(id, records)
+func (g *GRPCInterface) BatchInsert(ctx context.Context, batchReq *BatchInsertRequest) (*BatchInsertResponse, error) {
+	// 首先对数据进行一次预检，有问题及时返回
+	for _, insertReq := range batchReq.Inserts {
+		err := checkInsertReq(insertReq)
+		if err != nil {
+			if err == BadTimes {
+				return &BatchInsertResponse{
+					Status: ErrBadTimes,
+				}, nil
+			} else if err == BadUUID {
+				return &BatchInsertResponse{
+					Status: ErrBadUUID,
+				}, nil
+			}
+			return &BatchInsertResponse{ // 理论上不应该走到这儿
+				Status: &Status{
+					Code: -1,
+					Msg:  err.Error(),
+				},
+			}, nil
 		}
-	}(recordsMap)
+	}
+
+	batchCount := int(math.Ceil(float64(len(batchReq.Inserts)) / 100.0))
+	batches := make([][]*InsertRequest, batchCount)
+
+	for i := 0; i < batchCount; i++ {
+		from := i * 100
+		to := (i + 1) * 100
+		if to > len(batchReq.Inserts) {
+			to = len(batchReq.Inserts)
+		}
+		batches = append(batches, batchReq.Inserts[from:to])
+	}
+
+	for _, batch := range batches {
+		go g.batchInsert(batch)
+	}
 
 	return &BatchInsertResponse{
 		Status: Success,
@@ -249,4 +280,19 @@ func (g *GRPCInterface) QueryStatistics(ctx context.Context, req *QueryStatistic
 
 func checkTime(time int64) bool {
 	return time >= btrdb2.MinimumTime && time < btrdb2.MaximumTime
+}
+
+func checkInsertReq(req *InsertRequest) error {
+	_, err := uuid.ParseBytes(req.Uuid)
+	if err != nil {
+		log.Fatal("[checkInsertReq] invalid uuid: %v", err)
+		return BadUUID
+	}
+	for _, point := range req.Values {
+		if !checkTime(point.Time) {
+			log.Fatal("[checkInsertReq] invalid time: %v", point)
+			return BadTimes
+		}
+	}
+	return nil
 }
